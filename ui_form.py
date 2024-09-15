e77cff936b80215f7c5a2af6560125fe8d5799f6
Charles Santana <charlesstna@proton.me>
2024-09-15 15:27:51 -0300

    Update ui.form.py

--- ui_form.py
+++ ui_form.py
@@ -32,9 +32,6 @@ class Ui_Widget(object):
         self.criar_banco_sqlite = QPushButton(self.tab)
         self.criar_banco_sqlite.setObjectName(u"criar_banco_sqlite")
         self.criar_banco_sqlite.setGeometry(QRect(10, 20, 101, 24))
-        self.pushButton = QPushButton(self.tab)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(20, 60, 80, 24))
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QWidget()
         self.tab_2.setObjectName(u"tab_2")
@@ -48,7 +45,6 @@ class Ui_Widget(object):
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
         self.criar_banco_sqlite.setText(QCoreApplication.translate("Widget", u"Criar Banco", None))
-        self.pushButton.setText(QCoreApplication.translate("Widget", u"PushButton", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Widget", u"Configura\u00e7\u00f5es", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Widget", u"Tab 2", None))
     # retranslateUi
